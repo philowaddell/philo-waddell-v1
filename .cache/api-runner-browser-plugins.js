b312afa308c9f66828a9417d9b49d0c1bae19d46
Philo Waddell <philowad@live.co.uk>
2022-02-16 22:24:38 +0000

    Removed and cleaned public assets

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
-      options: {"plugins":[],"extensions":[".mdx"],"defaultLayouts":{},"gatsbyRemarkPlugins":[],"lessBabel":false,"remarkPlugins":[],"rehypePlugins":[],"mediaTypes":["text/markdown","text/x-markdown"],"root":"/home/philo/development/philo-waddell-v1","commonmark":false},
-    }]
